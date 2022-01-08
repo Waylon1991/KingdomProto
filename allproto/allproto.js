0b532c889de63b7c7334b6d061ff3aa1b889e828
zhanghaijun01 <zhanghaijun01@bianfeng.com>
2022-01-08 14:45:54 +0800

    协议发布脚本更新

--- allproto/allproto.js
+++ allproto/allproto.js
@@ -9,7 +9,8 @@ var allfilenames = ["client.proto", "common.proto", "error.proto"]
 var replacekey = ["common.", "errcode.", "\\[\\(validate.rules\\)"]
 var replacevalue = ["", "", ";/////[(validate.rules)"]
 //导出文件的路径
-var topath = "../DiceCC/protobuf/"
+var toprotopath = "../DiceCC/protobuf/"
+var tobinderpath = "../DiceCC/assets/scripts/net/pb/"
 var allprotocode = "";
 
 var root;
@@ -17,20 +18,20 @@ var root;
 function main() {
     allprotocode = 'syntax = "proto3";\n//package \n'
     root = __dirname.substring(0, __dirname.indexOf("allproto"));
-    if (topath.indexOf("..") == 0) {
-        topath = path.join(root, topath);
-        console.log(topath);
+    if (toprotopath.indexOf("..") == 0) {
+        toprotopath = path.join(root, toprotopath);
+        console.log(toprotopath);
     }
     readDir(root);
     for (var i = 0; i < replacekey.length; i++) {
         allprotocode = allprotocode.replace(eval("/" + replacekey[i] + "/g"), replacevalue[i]);
     }
 
-    fs.writeFile(topath + 'pb.proto', allprotocode, function (err) {
+    fs.writeFile(toprotopath + 'pb.proto', allprotocode, function (err) {
         if (err) throw err;
+        else console.log("写入结束" + toprotopath + 'pb.proto');
     });
-    console.log("压缩结束" + topath + 'pb.proto');
-
+     writePbBinder();
 }
 //文件遍历方法
 function readDir(filePath) {
@@ -75,5 +76,73 @@ function readProto(fPath) {
     let starindex = codeStr.indexOf("option go_package");
     starindex = codeStr.indexOf("\n", starindex) + 1;
     allprotocode += "////" + filename + "\n" + codeStr.substring(starindex);
+}
+
+function writePbBinder() {
+    let start = allprotocode.indexOf("enum CP {");
+    let end = allprotocode.indexOf("}", start);
+    let cpstr = allprotocode.substring(start, end);
+    const allcmdid = cpstr.split("\n");
+    let encodestr = "this.encodePB = {};\n"
+    let decodestr = "this.decodePB = {};\n"
+    for (let i = 0; i < allcmdid.length; i++) {
+        let cmdid = allcmdid[i];
+        let endindex = cmdid.indexOf("=");
+        if (endindex > -1) {
+
+            cmdid = cmdid.substring(0, endindex).trim();
+            let mess = cmdid.replace(eval("/_/g"), "");
+            let hascmd = allprotocode.indexOf(mess) != -1;
+            if (cmdid.indexOf("Req") != -1) {
+                if (hascmd) {
+                    encodestr += "        this.encodePB[pb.CP." + cmdid + "] = pb.encode" + mess + ";\n"
+                }
+            } else if (cmdid.indexOf("Resp") != -1 || cmdid.indexOf("Push") != -1) {
+                if (hascmd)
+                    decodestr += "        this.decodePB[pb.CP." + cmdid + "] = pb.decode" + mess + ";\n"
+            }
+        }
+    }
+    // console.log(encodestr)
+    // console.log(decodestr)
+    start = allprotocode.indexOf("enum ErrorType {");
+    end = allprotocode.indexOf("}", start);
+    cpstr = allprotocode.substring(start, end);
+    const allerror = cpstr.split("\n");
+    let errorstr = "this.errorMess = {};\n"
+    for (let i = 0; i < allerror.length; i++) {
+        let errid = allerror[i];
+        let endindex = errid.indexOf("=");
+        let startindex = errid.indexOf("//");
+        if (endindex > -1 && startindex > -1) {
+            err = errid.substring(0, endindex).trim();
+            mess = errid.substring(startindex + 2, errid.length).trim();
+            errorstr += "        this.errorMess[pb.ErrorType." + err + "] = \"" + mess + "\";\n"
+        }
+    }
+    // console.log(errorstr);
+    if (tobinderpath.indexOf("..") == 0) {
+        tobinderpath = path.join(root, tobinderpath);
+    }
+    let PbBinder = fs.readFileSync(tobinderpath + 'PbBinder.ts', 'utf8');
+    start = PbBinder.indexOf("this.encodePB = {};");
+    end = PbBinder.indexOf("}", start + 30);
+    oldstr = PbBinder.substring(start, end - 1);
+    // console.log(oldstr, encodestr)
+    PbBinder = PbBinder.replace(oldstr, encodestr);
+    start = PbBinder.indexOf("this.decodePB = {};");
+    end = PbBinder.indexOf("}", start + 30);
+    oldstr = PbBinder.substring(start, end - 1);
+    // console.log(oldstr, decodestr)
+    PbBinder = PbBinder.replace(oldstr, decodestr);
+    start = PbBinder.indexOf("this.errorMess = {};");
+    end = PbBinder.indexOf("}", start + 30);
+    oldstr = PbBinder.substring(start, end - 1);
+    // console.log(oldstr, errorstr)
+    PbBinder = PbBinder.replace(oldstr, errorstr);
+    fs.writeFile(tobinderpath + 'PbBinder.ts', PbBinder, function (err) {
+        if (err) throw err;
+        else console.log("写入结束" + tobinderpath + 'PbBinder.ts');
+    });
 }
 main();
